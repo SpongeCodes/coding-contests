ad3a09cf949674c0c2a5a67bb223c07eddc9c75c
Upamanyu Mukharji <upamanyu.mukharji@hotmail.com>
2024-06-09 23:23:24 +0530

    add alternate for 4

--- leetcode/biweekly/biweekly-132/3.cpp
+++ leetcode/biweekly/biweekly-132/3.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// there are more efficient solutions will add...
+
 class Solution {
 public:
     int maximumLength(vector<int>& nums, int k) {
--- leetcode/biweekly/biweekly-132/4.cpp
+++ leetcode/biweekly/biweekly-132/4.cpp
@@ -20,3 +20,30 @@ public:
        return lengths[k];
     }
 };
+// same idea.. dp[i][j] keeps track of subsequence ending at i with at most j differences
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+       // optimized from O(n^2*k) to O(n*k)
+       int n = nums.size();
+       vector<vector<int>> dp(n,vector<int>(k+1,1));
+       unordered_map<int,int> last;
+       // dp[i][j] => ending index i at most j different indices
+       vector<int> lengths(k+1,0); // maximum lengths 
+       // lengths[j] -> maximum length of subsequence with upto j differences
+       for(int i=0;i<n;++i) {
+            for(int j=k;j>=0;--j) {
+                // if you wanted to iterate in other order i.e 0 to k
+                // you would have to use previous iterations lengths[j-1] so lengths would be 2D to propagate
+                // have to find best result from previous index with k-1 differences
+                dp[i][j] = max(dp[i][j], 
+                1+(last.find(nums[i])==last.end()? 0:dp[last[nums[i]]][j]) );
+                if(j>0) dp[i][j] = max(dp[i][j], 1 + lengths[j-1] ); // this would be lengths[i-1][j-1]
+                lengths[j] = max(dp[i][j],lengths[j]); // this would become lengths[i][j]
+            }
+            last[nums[i]] = i;
+       }
+        return lengths[k];
+    }
+};
