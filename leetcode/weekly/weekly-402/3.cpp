83fb8369f776e4c3fa646b922b3c1e525d29fde1
Upamanyu Mukharji <upamanyu.mukharji@hotmail.com>
2024-06-16 13:34:18 +0530

    modify 3.cpp

--- leetcode/weekly/weekly-402/3.cpp
+++ leetcode/weekly/weekly-402/3.cpp
@@ -18,3 +18,56 @@ class Solution {
         return (*max_element(begin(dp), end(dp)))[0];
     }
 };
+
+// MY SOLUTION with indices same logic with deque
+using ll = long long;
+class Solution {
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        // YOU NEED DP
+       int n = power.size();
+       ll max_dp=0;
+       deque<pair<int,ll>> dq;
+       sort(power.begin(),power.end());
+       dq.push_back({0,power[0]});
+       // YOU CAN USE 2 POINTERS
+       // NOW OPTIMIZED SOLUTION IS TO USE DEQUEUE
+       for (int i = 1; i < power.size(); ++i) {
+            if(power[i]==power[dq.back().first]) {
+                ll newVal = dq.back().second + power[i];
+                dq.back() = {i,newVal};
+            }
+            // else go get max_dp upto i-2
+            else {
+                while(!dq.empty() && power[dq.front().first]+2<power[i]) {
+                    max_dp = max(max_dp,dq.front().second);
+                    dq.pop_front();
+                }
+                dq.push_back({i,max_dp+power[i]});
+            }
+       }
+       while(!dq.empty()) {
+            max_dp = max(max_dp,dq.front().second);
+            dq.pop_front();
+       } 
+       return max_dp;
+    }
+};
+
+/**
+ * PYTHON SOLUTION TAKE/NOT TAKE WILL NOT PASS IN CPP -CREDIT QIQI_IMPACT
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        power.sort()
+        @cache
+        def dp(idx, lst):
+            if idx == len(power):
+                return 0
+            v = power[idx]
+            ret = dp(idx+1, max(lst, v - 3))
+            if v - lst not in [1, 2]:
+                ret = max(ret, v + dp(idx+1, v))
+            return ret
+        return dp(0, -inf)
+        
+*/
